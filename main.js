f1da7293785b227c68c16f32a1892b1fd0d00876
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-10-24 12:46:44 -0600

    Add save messages function

--- main.js
+++ main.js
@@ -4,23 +4,30 @@ var submitButton = document.querySelector("#get-message");
 var messageDisplay = document.querySelector('p');
 var image = document.querySelector("#lil-human");
 var clearButton = document.querySelector("#clear-message");
+var favoriteButton = document.querySelector(".fav-button");
+var showFavoritesPage = document.querySelector(".show-favorites");
 
 submitButton.addEventListener('click', displayMessage);
 clearButton.addEventListener('click', clearMessage);
+favoriteButton.addEventListener('click', addToFavorites);
+//showFavoritesPage.addEventListener('click', displayFavorites);
 
+
+var favoritedMessages = [];
+var currentMessage;
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
 function chooseMantra() {
-  var currentMantra = mantras[getRandomIndex(mantras)];
-  messageDisplay.innerText = currentMantra;
+  messageDisplay.innerText = mantras[getRandomIndex(mantras)];
+  currentMessage = messageDisplay.innerText;
 };
 
 function chooseAffirmation() {
-  var currentAffirmation = affirmations[getRandomIndex(affirmations)];
-  messageDisplay.innerText = currentAffirmation;
+  messageDisplay.innerText = affirmations[getRandomIndex(affirmations)];
+  currentMessage = messageDisplay.innerText;
 };
 
 function displayMessage() {
@@ -29,9 +36,11 @@ function displayMessage() {
   image.classList.add("hidden");
   if(selectMantra.checked === true) {
       clearButton.classList.remove("hidden");
+      favoriteButton.classList.remove("hidden");
       return chooseMantra();
   } else if (selectAffirmation.checked === true) {
       clearButton.classList.remove("hidden");
+      favoriteButton.classList.remove("hidden");
       return chooseAffirmation();
   } else {
       messageDisplay.classList.add("hidden");
@@ -43,6 +52,13 @@ function clearMessage() {
   messageDisplay.classList.add("hidden");
   image.classList.remove("hidden");
   clearButton.classList.add("hidden");
+  favoriteButton.classList.add("hidden");
   selectMantra.checked = false;
   selectAffirmation.checked = false;
+};
+
+function addToFavorites() {
+  if(!favoritedMessages.includes(currentMessage)){
+    favoritedMessages.push(currentMessage);
+  }
 };
